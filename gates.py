abac6b4091f21e865ac88217b38e633a992a1dbf
Bogdan Bochkarev <bogdan.bochkariov@icloud.com>
2021-06-25 17:45:27 +0300

    Not private anymore

--- gates.py
+++ gates.py
@@ -14,46 +14,46 @@ class Gate:
     def __init__(self, gname: str, inum: int, onum: int, selnum=0, *inpvs):
         assert (len(inpvs) == inum or not inpvs), all(bool_portable(x) for x in inpvs)
         global current_gates_number
-        self.__gate_name = gname
-        self.__input_number = inum            # number of input channels in the gate
-        self.__output_number = onum           # number of output channels in the gate
-        self.__select_lines_number = selnum   # number of selectors in the gate
+        self._gate_name = gname
+        self._input_number = inum            # number of input channels in the gate
+        self._output_number = onum           # number of output channels in the gate
+        self._select_lines_number = selnum   # number of selectors in the gate
         if not inpvs:
-            self.__input_values = [None for _ in range(self.__input_number)]  # None is analogue of "X" state
+            self._input_values = [None for _ in range(self._input_number)]  # None is analogue of "X" state
         else:
-            self.__input_values = [bool(i) for i in inpvs]
-        self.__select_values = []                                              # todo! does it make any sense?
-        self.__out_connections_list = [{} for _ in range(self.__output_number)]  # list of (__output_number) dicts with
+            self._input_values = [bool(i) for i in inpvs]
+        self._select_values = []                                              # todo! does it make any sense?
+        self._out_connections_list = [{} for _ in range(self._output_number)]  # list of (_output_number) dicts with
         # connected gate as key and its input channel as value
-        self.__in_connections_list = [None for _ in range(self.__input_number)]  # list of (__input_number) connected
+        self._in_connections_list = [None for _ in range(self._input_number)]  # list of (_input_number) connected
         # Gates, where index is (input channel number - 1)
-        self.__output_values = [None for _ in range(self.__output_number)]
+        self._output_values = [None for _ in range(self._output_number)]
         current_gates_number += 1
 
     def func(self, sel=None):
-        assert None not in self.__input_values, "Function error: one input is either disconnected or invalid"
-        return self.__return_value(self.__output_values)
+        assert None not in self._input_values, "Function error: one input is either disconnected or invalid"
+        return self.__return_value(self._output_values)
 
     def connect(self, other, self_channel_number=None, other_channel_number=None):
         if self_channel_number is None:
-            self_channel_number = self.__output_number - 1
+            self_channel_number = self._output_number - 1
         else:
             self_channel_number -= 1
         connected = False
         if other_channel_number is None:
-            for i in range(len(other.__input_values)):
-                if other.__input_values[i] is None:
-                    other.__input_values[i] = self.func()[self_channel_number]
-                    self.__out_connections_list[self_channel_number][other] = i + 1
-                    other.__in_connections_list[i] = self
+            for i in range(len(other._input_values)):
+                if other._input_values[i] is None:
+                    other._input_values[i] = self.func()[self_channel_number]
+                    self._out_connections_list[self_channel_number][other] = i + 1
+                    other._in_connections_list[i] = self
                     connected = True
                     break
 
         else:
             other_channel_number -= 1
-            other.__input_values[other_channel_number] = self.func()[0]
-            self.__out_connections_list[self_channel_number][other] = other_channel_number + 1
-            other.__in_connections_list[other_channel_number] = self
+            other._input_values[other_channel_number] = self.func()[0]
+            self._out_connections_list[self_channel_number][other] = other_channel_number + 1
+            other._in_connections_list[other_channel_number] = self
             connected = True
 
         if not connected:
@@ -61,131 +61,131 @@ class Gate:
         # return connected
 
     def disconnect(self, other):
-        assert other in [j for sub in self.__out_connections_list for j in sub], \
+        assert other in [j for sub in self._out_connections_list for j in sub], \
             "Disconnection error: this gates were not connected"
 
         # Old version:
-        # for channel in range(len(other.__in_connections_list)):
-        #     if other.__in_connections_list[channel] is self:
-        #         other.__in_connections_list[channel] = None
-        #         other.__input_values[channel] = None
+        # for channel in range(len(other._in_connections_list)):
+        #     if other._in_connections_list[channel] is self:
+        #         other._in_connections_list[channel] = None
+        #         other._input_values[channel] = None
 
         # todo! write it more beautiful
-        for channel in range(len(self.__out_connections_list)):
-            for gate in self.__out_connections_list[channel]:
+        for channel in range(len(self._out_connections_list)):
+            for gate in self._out_connections_list[channel]:
                 if gate is other:
-                    other.__in_connections_list[self.__out_connections_list[channel][gate] - 1] = None
-                    other.__input_values[self.__out_connections_list[channel][gate] - 1] = None
-                    self.__out_connections_list[channel] = {}
+                    other._in_connections_list[self._out_connections_list[channel][gate] - 1] = None
+                    other._input_values[self._out_connections_list[channel][gate] - 1] = None
+                    self._out_connections_list[channel] = {}
 
     def change_signal(self, value):  # doesn't work with DMS
         assert isinstance(value, bool)
-        self.__output_values = [bool(value)]
-        for _ in range(len(self.__out_connections_list)):
-            if not len(self.__out_connections_list[0]):
+        self._output_values = [bool(value)]
+        for _ in range(len(self._out_connections_list)):
+            if not len(self._out_connections_list[0]):
                 return
-            connected_gate, num = list(self.__out_connections_list[0].keys())[0], \
-                                  list(self.__out_connections_list[0].values())[0]
+            connected_gate, num = list(self._out_connections_list[0].keys())[0], \
+                                  list(self._out_connections_list[0].values())[0]
             self.disconnect(connected_gate)
             self.connect(connected_gate, None, num)
             connected_gate.change_signal(connected_gate.func()[0])
 
     def __return_value(self, values):
-        assert all(isinstance(x, bool) for x in self.__output_values), "Output error: invalid output values"
-        for output_channel in range(self.__output_number):
-            for pair in self.__out_connections_list[output_channel]:
-                pair.__input_values[pair.__in_connections_list.index(self)] = values[output_channel]
+        assert all(isinstance(x, bool) for x in self._output_values), "Output error: invalid output values"
+        for output_channel in range(self._output_number):
+            for pair in self._out_connections_list[output_channel]:
+                pair._input_values[pair._in_connections_list.index(self)] = values[output_channel]
         return values  # fixme! it should look more beautiful
 
     def get_info(self):
-        ret = f"Gate name is '{self.__gate_name}'\n"\
-              f"Number of inputs: {self.__input_number}, number of outputs: {self.__output_number}\n\n"\
+        ret = f"Gate name is '{self._gate_name}'\n"\
+              f"Number of inputs: {self._input_number}, number of outputs: {self._output_number}\n\n"\
               f"This gate is in-connected to:\n"
-        for channel in range(len(self.__in_connections_list)):
+        for channel in range(len(self._in_connections_list)):
             ret += "\tChannel "+str(channel+1)+": "
-            if isinstance(self.__in_connections_list[channel], Gate):
-                ret += self.__in_connections_list[channel].gate_name + "\n"
+            if isinstance(self._in_connections_list[channel], Gate):
+                ret += self._in_connections_list[channel].gate_name + "\n"
             else:
                 ret += "unconnected\n"
         ret += "This gate out-connects to:\n"
-        for channel in range(len(self.__out_connections_list)):
+        for channel in range(len(self._out_connections_list)):
             ret += "\tChannel "+str(channel+1)+": "
-            for gate in self.__out_connections_list[channel]:
-                ret += f"\t\t{gate.__gate_name} to its in-channel {self.__out_connections_list[channel][gate]}\n"
+            for gate in self._out_connections_list[channel]:
+                ret += f"\t\t{gate._gate_name} to its in-channel {self._out_connections_list[channel][gate]}\n"
             else:
                 ret += "unconnected\n"
-        ret += f"\nInput values are: {self.__input_values}\n"
-        ret += f"Output values now are {self.__output_values}\n"
+        ret += f"\nInput values are: {self._input_values}\n"
+        ret += f"Output values now are {self._output_values}\n"
         return ret
 
 
 class OR(Gate):
     def __init__(self, gname="OR" + str(current_gates_number + 1), *inpvs):
         super().__init__(gname, 2, 1, *inpvs)
-        self.__output_values = [self.__input_values[0] or self.__input_values[1]]
+        self._output_values = [self._input_values[0] or self._input_values[1]]
 
     def func(self, sel=None):
-        self.__output_values = [self.__input_values[0] or self.__input_values[1]]
+        self._output_values = [self._input_values[0] or self._input_values[1]]
         return super(OR, self).func()
 
 
 class XOR(Gate):
     def __init__(self, gname="XOR"+str(current_gates_number + 1), *inpvs):
         super().__init__(gname, 2, 1, *inpvs)
-        self.__output_values = [self.__input_values[0] != self.__input_values[1]]
+        self._output_values = [self._input_values[0] != self._input_values[1]]
 
     def func(self, sel=None):
-        self.__output_values = [self.__input_values[0] != self.__input_values[1]]
+        self._output_values = [self._input_values[0] != self._input_values[1]]
         return super(XOR, self).func()
 
 
 class AND(Gate):
     def __init__(self, gname="AND"+str(current_gates_number + 1), *inpvs):
         super().__init__(gname, 2, 1, *inpvs)
-        self.__output_values = [self.__input_values[0] and self.__input_values[1]]
+        self._output_values = [self._input_values[0] and self._input_values[1]]
 
     def func(self, sel=None):
-        self.__output_values = [self.__input_values[0] and self.__input_values[1]]
+        self._output_values = [self._input_values[0] and self._input_values[1]]
         return super(AND, self).func()
 
 
 class NOT(Gate):
     def __init__(self, gname="NOT"+str(current_gates_number + 1), *inpvs):
         super().__init__(gname, 1, 1, *inpvs)
-        self.__output_values = [not self.__input_values[0]]
+        self._output_values = [not self._input_values[0]]
 
     def func(self, sel=None):
-        self.__output_values = [not self.__input_values[0]]
+        self._output_values = [not self._input_values[0]]
         return super(NOT, self).func()
 
 
 class NOR(Gate):
     def __init__(self, gname="NOR"+str(current_gates_number + 1), *inpvs):
         super().__init__(gname, 2, 1, *inpvs)
-        self.__output_values = [not(self.__input_values[0] or self.__input_values[1])]
+        self._output_values = [not(self._input_values[0] or self._input_values[1])]
 
     def func(self, sel=None):
-        self.__output_values = [not(self.__input_values[0] or self.__input_values[1])]
+        self._output_values = [not(self._input_values[0] or self._input_values[1])]
         return super(NOR, self).func()
 
 
 class NAND(Gate):
     def __init__(self, gname="NAND"+str(current_gates_number + 1), *inpvs):
         super().__init__(gname, 2, 1, *inpvs)
-        self.__output_values = [not (self.__input_values[0] and self.__input_values[1])]
+        self._output_values = [not (self._input_values[0] and self._input_values[1])]
 
     def func(self, sel=None):
-        self.__output_values = [not (self.__input_values[0] and self.__input_values[1])]
+        self._output_values = [not (self._input_values[0] and self._input_values[1])]
         return super(NAND, self).func()
 
 
 class BUF(Gate):
     def __init__(self, gname="BUF"+str(current_gates_number + 1), *inpvs):
         super().__init__(gname, 1, 1, *inpvs)
-        self.__output_values = [self.__input_values[0]]
+        self._output_values = [self._input_values[0]]
 
     def func(self, sel=None):
-        self.__output_values = [self.__input_values[0]]
+        self._output_values = [self._input_values[0]]
         return super(BUF, self).func()
 
 
@@ -193,7 +193,7 @@ class SND(Gate):
     def __init__(self, value=False, gname="SND" + str(current_gates_number + 1)):
         assert isinstance(value, bool)
         super().__init__(gname, 0, 1)
-        self.__output_values = [bool(value)]
+        self._output_values = [bool(value)]
 
 
 class RCV(Gate):
@@ -212,16 +212,16 @@ class MUX(Gate):
             selnum = ceil(log2(inum))
         super().__init__(gname, inum, 1, selnum, *inpvs)
         self.select_values = [0 for _ in range(0, selnum)]  # fixme! Does it make any sense?
-        self.__output_values = [None]
+        self._output_values = [None]
 
     def func(self, sel=None):
         if sel is None:
-            self.__output_values = [None]
+            self._output_values = [None]
         else:
             assert isinstance(sel, list), "MUX process failed: Selection signal is invalid"
-            assert len(sel) == self.__select_lines_number, "MUX process failed: Selection signal is invalid"
+            assert len(sel) == self._select_lines_number, "MUX process failed: Selection signal is invalid"
             sel = int(''.join([str(int(i)) for i in sel]), 2)
-            self.__output_values = [self.__input_values[sel]]
+            self._output_values = [self._input_values[sel]]
         return super(MUX, self).func()
 
 
@@ -229,10 +229,10 @@ class DMS(Gate):  # Decoder
     def __init__(self, gname="DMS" + str(current_gates_number + 1), inum=2, selnum=0, *inpvs):
         onum = 2 ** inum
         super().__init__(gname, inum, onum, selnum, *inpvs)
-        self.__output_values = [None for _ in range(self.__output_number)]
+        self._output_values = [None for _ in range(self._output_number)]
 
     def func(self, sel=None):
-        assert all(isinstance(x, bool) for x in self.__input_values), "DMS Process failed: wrong input"
-        sel = int(''.join([str(int(i)) for i in self.__input_values]), 2)
-        self.__output_values = [False if i != sel else True for i in range(self.__output_number)]
+        assert all(isinstance(x, bool) for x in self._input_values), "DMS Process failed: wrong input"
+        sel = int(''.join([str(int(i)) for i in self._input_values]), 2)
+        self._output_values = [False if i != sel else True for i in range(self._output_number)]
         return super(DMS, self).func()
